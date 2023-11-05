9e65607fe04d0e48f8907d9d376325ff82508d83
阮馨慧 <rxh20@mails.tsinghua.edu.cn>
2023-11-05 21:11:30 +0800

    web_monitor_change

--- quantify_utils/web_monitor2.py
+++ quantify_utils/web_monitor2.py
@@ -1,5 +1,4 @@
 import sys
-import time
 import panel as pn
 import quantify_core.data.handling as dh
 from quantify_core.data.handling import load_snapshot, to_gridded_dataset
@@ -10,7 +9,7 @@ import holoviews as hv  # noqa: F401
 
 
 from quantify_core.measurement.control import _DATASET_LOCKS_DIR
-from quantify_core.visualization.pyqt_plotmon_remote import _safe_load_dataset
+from quantify_core.visualization.pyqt_plotmon_remote import _safe_load_dataset, _last_modified
 from .visualization import heatmap_crange_cmap
 
 
@@ -34,44 +33,109 @@ class DataPlot:
         self.data_var = data_var
         self.draw_undone = draw_undone
         self.draw_done = draw_done
+        
+        self.is_finished = False
+        
+        self.dset_cache = _safe_load_dataset(self.tuid, _DATASET_LOCKS_DIR)
+        self.dset_last_modified = 0
+        
+        self.periodic_callback = None
+        
+        self.plot = pn.pane.panel(self.create_data_plot())
+        
+        self.interval = 1.0
+        self.run = True
 
-    def create_data_plot(self, interval, run, cmap):
-        is_finished = False
-        if self.tuid == "":
-            yield None
-        while True:
-            if not run:
-                yield "Calculation did not run yet"
+    def update_dset(self):
+        last_modified = _last_modified(self.tuid)
+        if last_modified > self.dset_last_modified:
+            self.dset_last_modified = last_modified
+            self.dset_cache = _safe_load_dataset(self.tuid, _DATASET_LOCKS_DIR)
+            return True
+        else:
+            return False
+        
+    def add_periodic_callback(self, *events):
+        # if not run:
+        #     self.periodic_callback.stop()
+        #     self.periodic_callback = None
+        for event in events:
+            if event.obj.name == "Update Interval":
+                self.interval = event.new
+            elif event.obj.name == "Press to run calculation":
+                self.run = event.new
+            else:
                 return
-            time.sleep(interval)
+        
+        if self.periodic_callback is not None:
+            self.periodic_callback.stop()
+        if self.run:
+            self.periodic_callback = pn.state.add_periodic_callback(self._callback, int(self.interval*1000))
+        else:
+            self.periodic_callback = None
+        
+    def _callback(self):
+        # update dataset
+        _modified = self.update_dset()
+        print("modified: ", _modified)
+        if not _modified:
+            return
+        else:
+            _fig = self.create_data_plot() 
+            print(hash(_fig))
+            self.plot = _fig
+            self.plot.param.trigger("object")
+            
+        
 
-            dset = _safe_load_dataset(self.tuid, _DATASET_LOCKS_DIR)
-            is_finished = not np.any(dset.y0.isnull().to_numpy())
+    def create_data_plot(self):
+        self.is_finished = not (list(self.dset_cache.data_vars.values())[0].isnull().any().values)
+        is_finished = self.is_finished
 
-            if dset.attrs["grid_2d"]:  # 2d-data
-                dset_grid = to_gridded_dataset(dset)
+        dset = self.dset_cache
+        
 
-                if not is_finished:
-                    fig = None
-                    if self.draw_undone is not None:
-                        fig = self.draw_undone(dset_grid)
-                    else:
-                        fig = self._default_draw_undone(dset_grid)
-                    yield pn.Column(fig)
+        if dset.attrs["grid_2d"]:  # 2d-data
+            dset_grid = to_gridded_dataset(dset)
+
+            if not is_finished:
+                fig = None
+                if self.draw_undone is not None:
+                    fig = self.draw_undone(dset_grid)
                 else:
-                    fig = None
-                    if self.draw_done is not None:
-                        fig = self.draw_done(dset_grid)
-                    else:
-                        fig = self._default_draw_done(dset_grid)
-                    yield pn.Column(fig, pn.pane.Str("Finished"))
-                # return
-            else:  # 1d-data
-                if not is_finished:
-                    if self.draw_undone is not None:
-                        fig = self.draw_undone(dset)
-                    else:
-                        fig = pn.pane.HoloViews(
+                    fig = self._default_draw_undone(dset_grid)
+                return pn.Column(fig)
+            else:
+                fig = None
+                if self.draw_done is not None:
+                    fig = self.draw_done(dset_grid)
+                else:
+                    fig = self._default_draw_done(dset_grid)
+                return pn.Column(fig, pn.pane.Str("Finished"))
+            # return
+        else:  # 1d-data
+            if not is_finished:
+                if self.draw_undone is not None:
+                    fig = self.draw_undone(dset)
+                else:
+                    fig = pn.pane.HoloViews(
+                        dset.hvplot.line(
+                            x="x0",
+                            y=self.data_var,
+                        )
+                        * dset.hvplot.scatter(
+                            x="x0",
+                            y=self.data_var,
+                        )
+                    )
+
+                return pn.Column(fig)
+            else:
+                if self.draw_done is not None:
+                    fig = self.draw_done(dset)
+                else:
+                    fig = pn.Column(
+                        pn.pane.HoloViews(
                             dset.hvplot.line(
                                 x="x0",
                                 y=self.data_var,
@@ -80,31 +144,12 @@ class DataPlot:
                                 x="x0",
                                 y=self.data_var,
                             )
-                        )
+                        ),
+                        pn.pane.Str("Finished"),
+                    )
 
-                    yield pn.Column(fig)
-                else:
-                    if self.draw_done is not None:
-                        fig = self.draw_done(dset)
-                    else:
-                        fig = pn.Column(
-                            pn.pane.HoloViews(
-                                dset.hvplot.line(
-                                    x="x0",
-                                    y=self.data_var,
-                                )
-                                * dset.hvplot.scatter(
-                                    x="x0",
-                                    y=self.data_var,
-                                )
-                            ),
-                            pn.pane.Str("Finished"),
-                        )
-
-                        yield fig
-
-            if is_finished:
-                return
+                    return fig
+        return fig
 
     def _default_draw_undone(self, dset):
         return dset.hvplot.image(
@@ -127,10 +172,10 @@ def snapshot_prune(snapshot):
             return {}
 
 
-def main(datadir, tuid, draw_undone=None, draw_done=None, snapshot_preprocess=None):
+def main(datadir, tuid, draw_undone=None, draw_done=None, snapshot_preprocess=None, port=5006):
     dh.set_datadir(datadir)
     template = pn.template.BootstrapTemplate(title="Web Monitor")
-    run = pn.widgets.Checkbox(name="Press to run calculation", align="center")
+    run = pn.widgets.Checkbox(name="Press to run calculation", align="center", value=False)
     update_interval = pn.widgets.FloatInput(name="Update Interval", value=1, start=0.05)
     shutdown_button = pn.widgets.Button(
         name="Shutdown the server", button_type="danger"
@@ -149,11 +194,20 @@ def main(datadir, tuid, draw_undone=None, draw_done=None, snapshot_preprocess=No
     # json snapshot `about`
     template.sidebar.append(create_json_pane(tuid, -1, snapshot_preprocess))
 
+    # create data panel
     data_plot = DataPlot(tuid, draw_undone=draw_undone, draw_done=draw_done)
-    plot = pn.bind(data_plot.create_data_plot, update_interval, run, "bwr")
-    template.main.append(plot)
+    
+    update_interval.param.watch(data_plot.add_periodic_callback, 'value')
+    run.param.watch(data_plot.add_periodic_callback, "value")
+    
+    
+    pn.Column
+    template.main.append(data_plot.plot)
+    update_interval.param.trigger("value")
+    run.param.trigger("value")
 
-    server = pn.serve(template, show=False, port=5006, threaded=True)
+    server = pn.serve(template, show=False, port=port, threaded=True, admin=True)
+
     server.join()
 
 
